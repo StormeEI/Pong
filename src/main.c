6ad6378cdfa1c401ea5c3d1c0ce18cb1365edee7
Cody Kaughman <cakaughman.101@gmail.com>
2025-01-24 18:15:21 -0600

    Added raylib

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include <main.h>
 #include <stdio.h>
+#include "raylib.h"
 
 int main() {
-    printf("Hello World!");
+    InitWindow(800,600,"shitFart");
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
+
+/*if(WIN32)
+    target_link_libraries(Pong Winmn.lib)
+elseif(UNIX)
+    target_link_libraries(Pong m pthread dl)
+endif()*/
